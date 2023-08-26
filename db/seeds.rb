92af8cebb74bc101b526054bbe377b6766421441
erika suzuki <erikasuzuki34@gmail.com>
2023-08-26 19:27:53 -0300

    task-manager done but without style

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,16 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Task.create!(
+  [
+    { title: 'Study', details: 'Study CSS', completed: false },
+    { title: 'Laundry', details: 'Do the laundry of penelope and scooby', completed: true },
+    { title: 'Clean', details: 'Clean the apartment', completed: false },
+    { title: 'Eat', details: 'Do not forget to eat the italian on the fridge otherwise its gonna rot', completed: true },
+    { title: 'Work out', details: 'Gym in about an hour', completed: false },
+    { title: 'Study for the interview', details: 'Study for the job interview of UEX', completed: true },
+    { title: 'Shower', details: 'You are stinking! Go shower', completed: false }
+  ]
+)
